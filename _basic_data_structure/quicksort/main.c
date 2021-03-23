b9b4f21ad98c06efd08179f1ad7ba031bd2abbd3
Ma Xeno <xeno.ma@cn.bosch.com>
2021-03-23 13:39:16 +0100

    Quicksort implementation
    - Implement quicksort with Hoare partition scheme

--- _basic_data_structure/quicksort/main.c
+++ _basic_data_structure/quicksort/main.c
@@ -1,6 +1,63 @@
 #include <stdio.h>
 
+#define MAX_INPUT (1000)
+
+int input[MAX_INPUT];
+int input_count;
+
+void swap(int *p1, int *p2)
+{
+    int temp;
+    temp = *p1;
+    *p1 = *p2;
+    *p2 = temp;
+}
+
+/* Hoare partition scheme with median-of-three pivot selection */
+int partition(int *input, int lo, int hi)
+{
+    int pivot;
+    int i, j;
+    pivot = input[(lo + hi) / 2];
+    if (pivot < input[lo] && pivot < input[hi])
+        pivot = input[lo] < input[hi] ? input[lo] : input[hi];
+    else if (input[lo] < pivot && input[hi] < pivot)
+        pivot = input[lo] < input[hi] ? input[hi] : input[lo];
+    i = lo;
+    j = hi;
+    while (1) {
+        while (input[i] < pivot)
+            ++i;
+        while (pivot < input[j])
+            --j;
+        if (i < j)
+            swap(&input[i], &input[j]);
+        else
+            return j;
+    }
+}
+
+void quicksort(int *input, int lo, int hi)
+{
+    int p;
+    if (lo < hi) {
+        p = partition(input, lo, hi);
+        quicksort(input, lo, p);
+        quicksort(input, p + 1, hi);
+    }
+}
+
 int main(void)
 {
+    int i;
+
+    input_count = 0;
+    while (scanf("%d", &input[input_count]) == 1)
+        ++input_count;
+    printf("input_count=%d\n", input_count);
+    quicksort(input, 10, input_count - 11);
+    for (i = 0; i < input_count; ++i)
+        printf("%d\n", input[i]);
+
     return 0;
 }

b64d3b3af19ba3d91b981165db491b7fbd66f2cf
Ma Xeno <xeno.ma@cn.bosch.com>
2021-04-22 14:46:23 +0200

    Delete logic
    - Add delete logic for red-black tree

--- _basic_data_structure/red_black_tree/main.c
+++ _basic_data_structure/red_black_tree/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
 
 #define MAX_INPUT (1000)
 #define NODE_POOL_SIZE (1000)
@@ -71,6 +72,27 @@ void release_node(struct rb_node *node)
     released_node_list = released_node;
 }
 
+void rotate(struct rb_tree *tree, struct rb_node *root, int side)
+{
+    struct rb_node *new_root;
+    new_root = root->child[side ^ 1];
+    assert(new_root != NULL);
+    if (root->parent == NULL)
+        tree->root = new_root;
+    else {
+        if (root == root->parent->child[0])
+            root->parent->child[0] = new_root;
+        else
+            root->parent->child[1] = new_root;
+    }
+    new_root->parent = root->parent;
+    root->child[side ^ 1] = new_root->child[side];
+    if (new_root->child[side] != NULL)
+        new_root->child[side]->parent = root;
+    new_root->child[side] = root;
+    root->parent = new_root;
+}
+
 struct rb_node * insert_from_node(struct rb_node *node, int key)
 {
     int side;
@@ -96,7 +118,6 @@ void insert(struct rb_tree *tree, int key)
     struct rb_node *parent;
     struct rb_node *g_parent;
     struct rb_node *uncle;
-    struct rb_node *gg_parent;
     int side;
 
     /* Normal insertion */
@@ -137,39 +158,223 @@ void insert(struct rb_tree *tree, int key)
         /* Case 4: Current node and parent at different side */
         if (node == parent->child[side ^ 1]) {
             /* Side rotation */
-            g_parent->child[side] = node;
-            node->parent = g_parent;
-            parent->child[side ^ 1] = node->child[side];
-            if (node->child[side] != NULL)
-                node->child[side]->parent = parent;
-            node->child[side] = parent;
-            parent->parent = node;
+            rotate(tree, parent, side);
             node = parent;
             parent = node->parent;
         }
         /* Case 5: Rotate parent to grand parent and re-paint */
         /* Side ^ 1 rotation */
-        gg_parent = g_parent->parent;
-        if (gg_parent == NULL)
-            tree->root = parent;
-        else {
-            if (g_parent == gg_parent->child[0])
-                gg_parent->child[0] = parent;
-            else
-                gg_parent->child[1] = parent;
-        }
-        parent->parent = gg_parent;
-        g_parent->child[side] = parent->child[side ^ 1];
-        if (parent->child[side ^ 1] != NULL)
-            parent->child[side ^ 1]->parent = g_parent;
-        parent->child[side ^ 1] = g_parent;
-        g_parent->parent = parent;
+        rotate(tree, g_parent, side ^ 1);
         g_parent->color = RED;
         parent->color = BLACK;
         return;
     } while (node->parent != NULL);
     /* Case 2: make sure root is black */
     node->color = BLACK;
+}
+
+struct rb_node * find(struct rb_tree *tree, int key)
+{
+    struct rb_node *node;
+    node = tree->root;
+    while (node != NULL) {
+        if (node->key == key)
+            return node;
+        if (key < node->key)
+            node = node->child[0];
+        else
+            node = node->child[1];
+    }
+    return NULL;
+}
+
+void node_swap(struct rb_tree *tree, struct rb_node *node1, struct rb_node *node2)
+{
+    struct rb_node *temp;
+    int side;
+    int color_temp;
+    if (node1 == node2)
+        return;
+    if (node1 == node2->parent) {
+        temp = node1;
+        node1 = node2;
+        node2 = temp;
+    }
+    if (node1->parent == node2) {
+        if (node1->child[0] != NULL)
+            node1->child[0]->parent = node2;
+        if (node1->child[1] != NULL)
+            node1->child[1]->parent = node2;
+        if (node1 == node2->child[0])
+            side = 0;
+        else
+            side = 1;
+        if (node2->child[side ^ 1] != NULL)
+            node2->child[side ^ 1]->parent = node1;
+        temp = node1->child[side ^ 1];
+        node1->child[side ^ 1] = node2->child[side ^ 1];
+        node2->child[side ^ 1] = temp;
+        node2->child[side] = node1->child[side];
+        node1->child[side] = node2;
+        temp = node2->parent;
+        if (temp != NULL) {
+            if (node2 == temp->child[0])
+                temp->child[0] = node1;
+            else
+                temp->child[1] = node1;
+        }
+        else
+            tree->root = node1;
+        node1->parent = temp;
+        node2->parent = node1;
+    }
+    else {
+        if (node1->child[0] != NULL)
+            node1->child[0]->parent = node2;
+        if (node2->child[0] != NULL)
+            node2->child[0]->parent = node1;
+        if (node1->child[1] != NULL)
+            node1->child[1]->parent = node2;
+        if (node2->child[1] != NULL)
+            node2->child[1]->parent = node1;
+        temp = node1->child[0];
+        node1->child[0] = node2->child[0];
+        node2->child[0] = temp;
+        temp = node1->child[1];
+        node1->child[1] = node2->child[1];
+        node2->child[1] = temp;
+        temp = node1->parent;
+        if (temp != NULL) {
+            if (node1 == temp->child[0])
+                temp->child[0] = node2;
+            else
+                temp->child[1] = node2;
+        }
+        else
+            tree->root = node2;
+        temp = node2->parent;
+        if (temp != NULL) {
+            if (node2 == temp->child[0])
+                temp->child[0] = node1;
+            else
+                temp->child[1] = node1;
+        }
+        else
+            tree->root = node1;
+        node2->parent = node1->parent;
+        node1->parent = temp;
+    }
+    color_temp = node1->color;
+    node1->color = node2->color;
+    node2->color = color_temp;
+}
+
+void delete_black_leaf_node(struct rb_tree *tree, struct rb_node *node)
+{
+    int side;
+    struct rb_node *parent;
+    struct rb_node *sibling;
+    struct rb_node *c_nephew; /* close nephew */
+    struct rb_node *d_nephew; /* distant nephew */
+    parent = node->parent;
+    assert(parent != NULL);
+    do {
+        if (node == parent->child[0])
+            side = 0;
+        else
+            side = 1;
+        sibling = parent->child[side ^ 1];
+        c_nephew = sibling->child[side];
+        assert(sibling != NULL);
+        if (sibling->color == RED) { /* case 3: sibling is red */
+            rotate(tree, parent, side);
+            sibling = c_nephew;
+            parent->color = RED;
+            parent->parent->color = BLACK;
+            /* Fall through to case 4 */
+        }
+        else {
+            d_nephew = sibling->child[side ^ 1];
+            /* case 5: distant nephew is black, close nephew is red */
+            if ((d_nephew== NULL || d_nephew->color == BLACK) && (c_nephew != NULL && c_nephew->color == RED)) {
+                rotate(tree, sibling, side ^ 1);
+                d_nephew = sibling;
+                sibling = c_nephew;
+                /*c_nephew = sibling->child[side];*/
+                d_nephew->color = RED;
+                sibling->color = BLACK;
+                /* Fall through to case 6 */
+            }
+            /* case 6: distant nephew is red */
+            if (d_nephew!= NULL && d_nephew->color == RED) {
+                rotate(tree, parent, side);
+                d_nephew->color = BLACK;
+                sibling->color = parent->color;
+                parent->color = BLACK;
+                return;
+            }
+        }
+        /* case 4: parent is red */
+        if (parent->color == RED) {
+            parent->color = BLACK;
+            sibling->color = RED;
+            return;
+        }
+        /* case 1: parent / sibling / nephews are all black */
+        sibling->color = RED;
+        node = parent;
+        parent = node->parent;
+    } while (parent != NULL);
+    /* case 2: sibling from every level is repainted red */
+}
+
+void delete(struct rb_tree *tree, int key)
+{
+    struct rb_node *node;
+    struct rb_node *temp;
+    node = find(tree, key);
+    if (node == NULL)
+        return;
+    /* Simple cases */
+    /* Root has no children */
+    if (tree->root->child[0] == NULL && tree->root->child[1] == NULL) {
+        release_node(tree->root);
+        tree->root = NULL;
+        return;
+    }
+    /* Node has 2 non-NULL children */
+    if (node->child[0] != NULL && node->child[1] != NULL) {
+        temp = node->child[0];
+        while (temp->child[1] != NULL)
+            temp = temp->child[1];
+        node_swap(tree, node, temp);
+    }
+    /* Node has one red child */
+    if (node->child[0] != NULL) {
+        node_swap(tree, node, node->child[0]);
+        assert(node->color == RED);
+    }
+    if (node->child[1] != NULL) {
+        node_swap(tree, node, node->child[1]);
+        assert(node->color == RED);
+    }
+    assert(node->child[0] == NULL && node->child[1] == NULL);
+    /* Red leaf node is directly removed */
+    if (node->color == RED) {
+        if (node == node->parent->child[0])
+            node->parent->child[0] = NULL;
+        else
+            node->parent->child[1] = NULL;
+        release_node(node);
+        return;
+    }
+    /* Black leaf node: complicated case */
+    delete_black_leaf_node(tree, node);
+    if (node == node->parent->child[0])
+        node->parent->child[0] = NULL;
+    else
+        node->parent->child[1] = NULL;
+    release_node(node);
 }
 
 void print_tree_pre_order(struct rb_node *node)
@@ -351,7 +556,10 @@ void print_tree_by_shape_fallen_in_order(struct rb_node *node)
         return;
     print_tree_by_shape_fallen_in_order(node->child[0]);
     print_parent_connector_in_order(node, NULL);
-    printf("%2d%c", node->key, node->color == BLACK ? 'B' : 'R');
+    if (node->color == BLACK)
+        printf("%2dB", node->key);
+    else
+        printf("\033[0;31m%2dR\033[0m", node->key);
     if (node->child[0] != NULL) {
         if (node->child[1] != NULL)
             printf("─┤");
@@ -388,6 +596,9 @@ int main(void)
     tree.root = NULL;
     for (i = 0; i < input_count; ++i)
         insert(&tree, input[i]);
+
+    delete(&tree, 8);
+    insert(&tree, 8);
 
     print_tree(&tree);
 

e2602387d7c3e1d557fa5b6032e3bd8ed5a06a38
zenomyth <zenomyth@hotmail.com>
2021-04-28 23:09:04 +0200

    Functions to draw lines and points
    - Separate functions to draw lines and points

--- 34-RUNAWAY-/test2/draw_asterisk/main.c
+++ 34-RUNAWAY-/test2/draw_asterisk/main.c
@@ -1,5 +1,13 @@
 #include <windows.h>
 #include <math.h>
+#include <stdio.h>
+
+struct point_f {
+	float x;
+	float y;
+};
+
+int cxClient, cyClient;
 
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 
@@ -45,14 +53,67 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 	return msg.wParam;
 }
 
+void convert_point_to_pixel(struct point_f *p_f, POINT *p)
+{
+	int midx = cxClient / 2;
+	int midy = cyClient / 2;
+	float unit = (float)(cxClient > cyClient ? cyClient : cxClient) / 30;
+	p->x = midx + p_f->x * unit;
+	p->y = midy - p_f->y * unit;
+}
+
+void draw_point(HDC hdc, struct point_f *p_f)
+{
+	HBRUSH hbrush;
+	HBRUSH hbrushOld;
+	POINT p;
+
+	hbrush = (HBRUSH)GetStockObject(BLACK_BRUSH);
+	hbrushOld = SelectObject(hdc, hbrush);
+	convert_point_to_pixel(p_f, &p);
+	Ellipse(hdc, p.x - 2, p.y - 2, p.x + 2, p.y + 2);
+	SelectObject(hdc, hbrushOld);
+}
+
+void draw_line(HDC hdc, struct point_f *pf1, struct point_f *pf2)
+{
+	POINT p;
+	convert_point_to_pixel(pf1, &p);
+	MoveToEx(hdc, p.x, p.y, NULL);
+	convert_point_to_pixel(pf2, &p);
+	LineTo(hdc, p.x, p.y);
+}
+
+void draw_plots(HDC hdc)
+{
+	struct point_f p1 = {-2, 3};
+	struct point_f p2 = {3, 1};
+	float k;
+
+	draw_point(hdc, &p1);
+	draw_point(hdc, &p2);
+
+	draw_line(hdc, &p1, &p2);
+	// MoveToEx(hdc, midx + p1.x * unit, midy - p1.y * unit, NULL);
+	// LineTo(hdc, midx + p2.x * unit, midy - p2.y * unit);
+
+	k = -(p2.x - p1.x) / (p2.y - p1.y);
+	printf("k=%f\n", k);
+
+	/*for(i = 0; i < NUM; i++)
+	{
+	   apt[i].x = i * cxClient / NUM;
+	   apt[i].y =(int)(cyClient / 2 *(1 - sin(TWOPI * i / NUM)));
+	}
+
+	Polyline(hdc, apt, NUM);*/
+}
+
 LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
-	static int  cxClient, cyClient;
 	HDC         hdc;
 	int         i;
 	PAINTSTRUCT ps;
-	HBRUSH hbrush;
-	HBRUSH hbrushOld;
 	//POINT       apt [NUM];
 
 	switch (message) {
@@ -63,23 +124,7 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
 
 	case WM_PAINT:
 		hdc = BeginPaint(hwnd, &ps);
-
-		MoveToEx(hdc, 0,        cyClient / 2, NULL);
-		LineTo  (hdc, cxClient, cyClient / 2);
-
-		// SetPixel(hdc, cxClient / 4, cyClient / 4, RGB(0, 0, 255));
-		hbrush = (HBRUSH)GetStockObject(BLACK_BRUSH);
-		hbrushOld = SelectObject(hdc, hbrush);
-		Ellipse(hdc, cxClient / 4 - 3, cyClient / 4 - 3, cxClient / 4 + 3, cyClient / 4 + 3);
-		SelectObject(hdc, hbrushOld);
-
-		/*for(i = 0; i < NUM; i++)
-		{
-		   apt[i].x = i * cxClient / NUM;
-		   apt[i].y =(int)(cyClient / 2 *(1 - sin(TWOPI * i / NUM)));
-		}
-
-		Polyline(hdc, apt, NUM);*/
+		draw_plots(hdc);
 		return 0;
 
 	case WM_DESTROY:

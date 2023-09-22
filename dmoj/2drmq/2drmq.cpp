7f247c5373857be29677eaf3be96161ce462cc55
Ma Xeno <bo.ma@bosch-sensortec.com>
2023-09-22 19:53:16 +0200

    Input as array
    - Assign input to an array

--- dmoj/2drmq/2drmq.cpp
+++ dmoj/2drmq/2drmq.cpp
@@ -8,19 +8,20 @@
 
 using namespace std;
 
+int data_2d[N][N];
 int n;
 int pow2[] = {1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024};
-int pow2_rmq[N][N][L][L];
+int pow2_rmq[L][L][N][N];
 
-void calc_pow2_rmq_1d(const std::vector<std::vector<int>>& arr, int idx1, int idx2)
+void calc_pow2_rmq_1d(int idx1, int idx2)
 {
     int min_val = INF;
     int pow2_idx = 0;
     for (int i = idx2; i < n; ++i) {
-        if (arr[idx1][i] < min_val)
-            min_val = arr[idx1][i];
+        if (data_2d[idx1][i] < min_val)
+            min_val = data_2d[idx1][i];
         if (i - idx2 + 1 == pow2[pow2_idx]) {
-            pow2_rmq[idx1][idx2][0][pow2_idx] = min_val;
+            pow2_rmq[0][pow2_idx][idx1][idx2] = min_val;
             if (idx2 + pow2[++pow2_idx] > n)
                 break;
         }
@@ -33,10 +34,10 @@ void pow2_rmq_v_popu(int idx1, int idx2) {
         int min_val = INF;
         int pow2_idx = 0;
         for (int i = idx1; i < n; ++i) {
-            if (pow2_rmq[i][idx2][0][pow2_idx2] < min_val)
-                min_val = pow2_rmq[i][idx2][0][pow2_idx2];
+            if (pow2_rmq[0][pow2_idx2][i][idx2] < min_val)
+                min_val = pow2_rmq[0][pow2_idx2][i][idx2];
             if (i - idx1 + 1 == pow2[pow2_idx]) {
-                pow2_rmq[idx1][idx2][pow2_idx][pow2_idx2] = min_val;
+                pow2_rmq[pow2_idx][pow2_idx2][idx1][idx2] = min_val;
                 if (idx1 + pow2[++pow2_idx] > n)
                     break;
             }
@@ -45,12 +46,21 @@ void pow2_rmq_v_popu(int idx1, int idx2) {
     } while (idx2 + pow2[pow2_idx2] <= n);
 }
 
-void init(std::vector<std::vector<int>> arr)
+void init(vector<vector<int>> arr)
 {
     n = arr.size();
+    int i = 0;
+    for (vector<vector<int>>::iterator itvv = arr.begin(); itvv != arr.end(); ++itvv, ++i) {
+        int j = 0;
+        for (vector<int>::iterator it = itvv->begin(); it != itvv->end(); ++it, ++j)
+            data_2d[i][j] = *it;
+    }
+    // for (int i = 0; i < n; ++i)
+        // for (int j = 0; j < n; ++j)
+            // data_2d[i][j] = arr[i][j];
     for (int i = 0; i < n; ++i)
         for (int j = 0; j < n; ++j)
-            calc_pow2_rmq_1d(arr, i, j);
+            calc_pow2_rmq_1d(i, j);
     for (int i = 0; i < n; ++i)
         for (int j = 0; j < n; ++j)
             pow2_rmq_v_popu(i, j);
@@ -66,10 +76,10 @@ int query(int a, int b, int c, int d)
         ++hh;
     while (pow2[ww + 1] < w)
         ++ww;
-    int sw = pow2_rmq[a][c][hh][ww];
-    int nw = pow2_rmq[b - pow2[hh] + 1][c][hh][ww];
-    int ne = pow2_rmq[b - pow2[hh] + 1][d - pow2[ww] + 1][hh][ww];
-    int se = pow2_rmq[a][d - pow2[ww] + 1][hh][ww];
+    int sw = pow2_rmq[hh][ww][a][c];
+    int nw = pow2_rmq[hh][ww][b - pow2[hh] + 1][c];
+    int ne = pow2_rmq[hh][ww][b - pow2[hh] + 1][d - pow2[ww] + 1];
+    int se = pow2_rmq[hh][ww][a][d - pow2[ww] + 1];
     return min({sw, nw, ne, se});
 }
 
@@ -78,7 +88,7 @@ int query(int a, int b, int c, int d)
     // int min_val = INF;
     // for (int i = a; i <= b; ++i)
         // for (int j = c; j <= d; ++j)
-            // if (range[i][j] < min_val)
-                // min_val = range[i][j];
+            // if (data[i][j] < min_val)
+                // min_val = data[i][j];
     // return min_val;
 // }
--- dmoj/2drmq/main.cpp
+++ dmoj/2drmq/main.cpp
@@ -3,8 +3,26 @@
 
 using namespace std;
 
+#define N 1000
+#define L 10
+
 void init(std::vector<std::vector<int>> arr);
 int query(int a, int b, int c, int d);
+extern int data_2d[N][N];
+extern int n;
+extern int pow2[];
+extern int pow2_rmq[L][L][N][N];
+
+void print_pow2_rmq()
+{
+    for (int i = 0; pow2[i] <= n; ++i)
+        for (int j = 0; pow2[j] <= n; ++j)
+            for (int l = 0; l < n; ++l)
+                for (int m = 0; m < n; ++m) {
+                    if (l + pow2[i] <= n && m + pow2[j] <= n)
+                        cout << ">> " << i << " " << j << " " << l << " " << m << " " << pow2_rmq[i][j][l][m] << endl;
+                }
+}
 
 int main()
 {
@@ -22,5 +40,6 @@ int main()
 		cin >> a >> b >> c >> d;
 		cout << query(a, b, c, d) << endl;
 	}
+    print_pow2_rmq();
     return 0;
 }

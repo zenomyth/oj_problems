6891f3a11c833506f921adc470e18e5c6e80723c
Ma Xeno <bo.ma@bosch-sensortec.com>
2023-10-05 16:20:55 +0200

    Linear buffer
    - Change to linear buffer

--- dmoj/aac1p4/main.cpp
+++ dmoj/aac1p4/main.cpp
@@ -12,6 +12,14 @@ struct occurrence_list {
 } pool[MAX_N];
 struct occurrence_list *watermark = pool;
 struct occurrence_list *ol[MAX_A + 1];
+int val[MAX_A];
+int val_len = 0;
+int pool_linear[MAX_N + 1];
+int watermark_linear = 1;
+struct {
+    int beg;
+    int end;
+} ol_linear[MAX_A + 1];
 
 int main()
 {
@@ -20,37 +28,50 @@ int main()
         cin >> a;
         watermark->idx = i;
         watermark->next = ol[a];
+        if (ol[a] == NULL)
+            val[val_len++] = a;
         ol[a] = watermark;
         ++watermark;
     }
+    for (int i = 0; i < val_len; ++i) {
+        a = val[i];
+        struct occurrence_list *p = ol[a];
+        ol_linear[a].beg = watermark_linear;
+        while (p != NULL) {
+            pool_linear[watermark_linear++] = p->idx;
+            p = p->next;
+        }
+        ol_linear[a].end = watermark_linear;
+    }
     do {
         cin >> l >> r >> x;
-        struct occurrence_list *p = NULL;
+        bool match_found = false;
         for (int f = 1; f * f < x; ++f) {
             int g = x / f;
             if (f * g != x)
                 continue;
-            p = ol[f];
-            while (p != NULL) {
-                if (p->idx >= l && p->idx <= r)
+            for (int i = ol_linear[f].beg; i < ol_linear[f].end; ++i) {
+                if (pool_linear[i] >= l && pool_linear[i] <= r) {
+                    match_found = true;
                     break;
-                p = p->next;
+                }
             }
-            if (p == NULL)
+            if (!match_found)
                 continue;
-            p = ol[g];
-            while (p != NULL) {
-                if (p->idx >= l && p->idx <= r)
+            match_found = false;
+            for (int i = ol_linear[g].beg; i < ol_linear[g].end; ++i) {
+                if (pool_linear[i] >= l && pool_linear[i] <= r) {
+                    match_found = true;
                     break;
-                p = p->next;
+                }
             }
-            if (p != NULL)
+            if (match_found)
                 break;
         }
-        if (p == NULL)
-            cout << "NO" << endl;
-        else
+        if (match_found)
             cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
     } while (--Q > 0);
     return 0;
 }

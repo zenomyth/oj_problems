04539dbc7fb91ad4da73fd799fcbdbe9d3a8feb2
Ma Xeno <bo.ma@bosch-sensortec.com>
2023-10-05 17:27:56 +0200

    Match finder function
    - Separate the match finder function

--- dmoj/aac1p4/main.cpp
+++ dmoj/aac1p4/main.cpp
@@ -5,43 +5,46 @@
 
 using namespace std;
 
-int N, Q, a, l, r, x;
-struct occurrence_list {
-    int idx;
-    struct occurrence_list *next;
-} pool[MAX_N];
-struct occurrence_list *watermark = pool;
-struct occurrence_list *ol[MAX_A + 1];
+int N, Q, l, r, x;
+int arr[MAX_N + 1];
 int val[MAX_A];
 int val_len = 0;
-int pool_linear[MAX_N + 1];
-int watermark_linear = 1;
 struct {
     int beg;
-    int end;
-} ol_linear[MAX_A + 1];
+    int len;
+} ol[MAX_A + 1];
+int pool[MAX_N];
+
+bool find_matching_index(int a)
+{
+    // for (int i = 0; i < ol[a].len; ++i) {
+    for (int i = ol[a].len - 1; i >= 0; --i) {
+        if (pool[ol[a].beg + i] >= l && pool[ol[a].beg + i] <= r)
+            return true;
+    }
+    return false;
+}
 
 int main()
 {
     cin >> N >> Q;
     for (int i = 1; i <= N; ++i) {
+        int a;
         cin >> a;
-        watermark->idx = i;
-        watermark->next = ol[a];
-        if (ol[a] == NULL)
+        arr[i] = a;
+        if (ol[a].len == 0)
             val[val_len++] = a;
-        ol[a] = watermark;
-        ++watermark;
+        ++ol[a].len;
     }
-    for (int i = 0; i < val_len; ++i) {
-        a = val[i];
-        struct occurrence_list *p = ol[a];
-        ol_linear[a].beg = watermark_linear;
-        while (p != NULL) {
-            pool_linear[watermark_linear++] = p->idx;
-            p = p->next;
-        }
-        ol_linear[a].end = watermark_linear;
+    for (int i = 0, beg = 0; i < val_len; ++i) {
+        int a = val[i];
+        ol[a].beg = beg;
+        beg += ol[a].len;
+        ol[a].len = 0;
+    }
+    for (int i = 1; i <= N; ++i) {
+        int a = arr[i];
+        pool[ol[a].beg + ol[a].len++] = i;
     }
     do {
         cin >> l >> r >> x;
@@ -50,23 +53,10 @@ int main()
             int g = x / f;
             if (f * g != x)
                 continue;
-            for (int i = ol_linear[f].beg; i < ol_linear[f].end; ++i) {
-                if (pool_linear[i] >= l && pool_linear[i] <= r) {
-                    match_found = true;
-                    break;
-                }
-            }
-            if (!match_found)
-                continue;
-            match_found = false;
-            for (int i = ol_linear[g].beg; i < ol_linear[g].end; ++i) {
-                if (pool_linear[i] >= l && pool_linear[i] <= r) {
-                    match_found = true;
-                    break;
-                }
-            }
-            if (match_found)
+            if (find_matching_index(f) && find_matching_index(g)) {
+                match_found = true;
                 break;
+            }
         }
         if (match_found)
             cout << "YES" << endl;

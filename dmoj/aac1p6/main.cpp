5441b66fe15b9e374a5431e6c80afbfedf4ee2f4
Ma Xeno <bo.ma@bosch-sensortec.com>
2023-10-10 12:20:24 +0200

    Fenwick tree solution
    - Implement the Fenwick tree solution
    - Got RTE in the last case. Guess it's due to inadequate memory

--- dmoj/aac1p6/main.cpp
+++ dmoj/aac1p6/main.cpp
@@ -2,11 +2,85 @@
 #include <vector>
 #include <utility>
 #include <algorithm>
+#include <queue>
 
 using namespace std;
 
 int n;
 vector<pair<int, int>> alpacas;
+vector<int> dp;
+vector<int> fenwick;
+priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> waiting_list;
+
+void fenwick_insert(int pos, int val)
+{
+    int size = (int)fenwick.size();
+    while (pos < size) {
+        if (fenwick[pos] < val)
+            fenwick[pos] = val;
+        pos += pos & (-pos);
+    }
+}
+
+int fenwick_query(int pos)
+{
+    int val = 0;
+    while (pos > 0) {
+        if (val < fenwick[pos])
+            val = fenwick[pos];
+        pos -= pos & (-pos);
+    }
+    return val;
+}
+
+void find_participants()
+{
+    sort(alpacas.begin(), alpacas.end());
+    // for (auto &it: alpacas)
+        // cout << it.first << " " << it.second << endl;
+    dp.reserve(n + 1);
+    int ans = 1;
+    fenwick.resize(alpacas[n - 1].first + 1, 0);
+    for (int i = 0; i < n; ++i) {
+        // cout << alpacas[i].first << " " << alpacas[i].second << endl;
+        while (!waiting_list.empty()) {
+            const pair<int, int> &top = waiting_list.top();
+            if (top.first > alpacas[i].first)
+                break;
+            fenwick_insert(alpacas[top.second].first, dp[top.second]);
+            // cout << top.first << " " << top.second << endl;
+            waiting_list.pop();
+        }
+        dp[i] = fenwick_query(alpacas[i].first - alpacas[i].second) + 1;
+        if (ans < dp[i])
+            ans = dp[i];
+        waiting_list.push(make_pair(alpacas[i].first + alpacas[i].second, i));
+        // cout << dp[i] << endl;
+    }
+    cout << ans << endl;
+}
+
+void test_fenwick()
+{
+    fenwick.resize(11, 0);
+    fenwick_insert(3, 7);
+    fenwick_insert(2, 5);
+    fenwick_insert(5, 5);
+    for (int i = 1; i < (int)fenwick.size(); ++i)
+        cout << fenwick[i] << " " << fenwick_query(i) << endl;
+}
+
+void test_priority_queue()
+{
+    waiting_list.push(make_pair(5, 1));
+    waiting_list.push(make_pair(3, 2));
+    waiting_list.push(make_pair(6, 3));
+    waiting_list.push(make_pair(2, 4));
+    while (!waiting_list.empty()) {
+        cout << waiting_list.top().first << " " << waiting_list.top().second << endl;
+        waiting_list.pop();
+    }
+}
 
 int main()
 {
@@ -17,24 +91,8 @@ int main()
         cin >> a >> b;
         alpacas.push_back(make_pair(a, b));
     }
-    sort(alpacas.begin(), alpacas.end());
-    // for (auto &it: alpacas)
-        // cout << it.first << " " << it.second << endl;
-    vector<int> dp;
-    dp.reserve(n);
-    dp[0] = 1;
-    int ans = 1;
-    for (int i = 1; i < n; ++i) {
-        int cc = 0;
-        for (int j = 0; j < i; ++j) {
-            if (alpacas[i].first - alpacas[j].first >= max(alpacas[i].second, alpacas[j].second) && cc < dp[j])
-                cc = dp[j];
-        }
-        dp[i] = cc + 1;
-        if (ans < dp[i])
-            ans = dp[i];
-        // cout << dp[i] << endl;
-    }
-    cout << ans << endl;
+    find_participants();
+    // test_fenwick();
+    // test_priority_queue();
     return 0;
 }

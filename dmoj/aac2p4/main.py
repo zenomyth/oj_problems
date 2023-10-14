b5b4841a679e445ecc2e857e2625464d5ff8ad7b
zenomyth <zenomyth@hotmail.com>
2023-10-15 01:53:19 +0200

    Python implementation
    - Save logic got AC with python

---
+++ dmoj/aac2p4/main.py
@@ -0,0 +1,14 @@
+t = int(input())
+for x in range(t):
+    n = int(input())
+    a = [int(xx) for xx in input().split()]
+    if n == 1:
+        print(a[0])
+    a.sort()
+    if (n & 1) == 0 and a[0] == a[n - 1]:
+        print(-1)
+        continue
+    print(a[n - 1], end = " ")
+    for xx in range(1, n - 1):
+        print(a[xx], end = " ")
+    print(a[0]);

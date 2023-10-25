ed6234506e0833890f1330ea3a36e137d609c640
Ma Xeno <bo.ma@bosch-sensortec.com>
2023-10-25 18:59:41 +0200

    HLD + BIT first try
    - Implement HLD
    - Implement BIT (Fenwick) for direct count sum
    - Got TLE even on case #2. Guess more clever BIT implementation is needed

--- dmoj/aac2p6/main.cpp
+++ dmoj/aac2p6/main.cpp
@@ -10,14 +10,108 @@ struct list_item {
     struct list_item *next;
 };
 struct list_item *al[N + 1], pool[N * 2], *wm = pool;
+int parent[N + 1];
+int heavy[N + 1];
+int depth[N + 1];
+int head[N + 1];
+int len_from_head[N + 1];
+int pos[N], next_pos = 0;
+int fenwick[N];
+
+int calc_weight_and_depth(int v)
+{
+    int size = 1, hc = 0, hc_size = 0;
+    for (struct list_item *c = al[v]; c != NULL; c = c->next) {
+        if (c->v == parent[v])
+            continue;
+        parent[c->v] = v;
+        depth[c->v] = depth[v] + 1;
+        int size1 = calc_weight_and_depth(c->v);
+        if (size1 > hc_size) {
+            hc_size = size1;
+            hc = c->v;
+        }
+        size += size1;
+    }
+    if (2 * hc_size >= size)
+        heavy[v] = hc;
+    return size;
+}
+
+void build_hld(int v, int h)
+{
+    head[v] = h;
+    ++len_from_head[h];
+    pos[v] = next_pos++;
+    if (heavy[v] != 0)
+        build_hld(heavy[v], h);
+    for (struct list_item *c = al[v]; c != NULL; c = c->next) {
+        if (c->v == parent[v] || c->v == heavy[v])
+            continue;
+        build_hld(c->v, c->v);
+    }
+}
 
 void preprocess()
 {
+    calc_weight_and_depth(1);
+    heavy[1] = 0;
+    build_hld(1, 1);
+    // for (int i = 1; i <= n; ++i)
+        // printf("%d: d=%d, h=%d, p=%d, head=%d, pos=%d\n", i, depth[i], heavy[i], parent[i], head[i], pos[i]);
+}
+
+void fenwick_add(int v, int val)
+{
+    int h = head[v];
+    while (true) {
+        int beg = pos[h];
+        int fpos = pos[v] - beg + 1;
+        while (fpos > 0) {
+            fenwick[beg - 1 + fpos] += val;
+            fpos -= fpos & (-fpos);
+        }
+        if (h == 1)
+            break;
+        v = parent[h];
+        h = head[v];
+    }
+}
+
+int fenwick_query(int v)
+{
+    int val = 0;
+    int h = head[v];
+    int beg = pos[h];
+    int fpos = pos[v] - beg + 1;
+    while (fpos <= len_from_head[h]) {
+        val += fenwick[beg - 1 + fpos];
+        fpos += fpos & (-fpos);
+    }
+    // printf("query(%d)=%d\n", v, val);
+    return val;
 }
 
 long long query_min_time()
 {
-    return 0;
+    long long t = 0, ts = 0;
+    for (int i = 0; i < x; ++i) {
+        t += (long long)depth[xa[i]];
+        fenwick_add(xa[i], 1);
+    }
+    // for (int i = 0; i < n; ++i)
+        // printf("f[%d]=%d\n", i, fenwick[i]);
+    for (int i = 0; i < x; ++i) {
+        long long ts1 = 0;
+        for (int v = xa[i]; v != 1; v = parent[v])
+            ts1 += (long long)fenwick_query(v);
+        if (ts1 > ts)
+            ts = ts1;
+    }
+    // printf("%lld %lld\n", t, ts);
+    for (int i = 0; i < x; ++i)
+        fenwick_add(xa[i], -1);
+    return t - ts;
 }
 
 int main()
